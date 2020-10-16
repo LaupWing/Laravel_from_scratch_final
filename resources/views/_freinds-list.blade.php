4f19716ba259bcaf518a13e673811c52ad8f3e20
LaupWing <loc_strict@hotmail.com>
2020-10-16 15:34:48 +0200

    friends

---
+++ resources/views/_freinds-list.blade.php
@@ -0,0 +1,13 @@
+<h3>Friends</h3>
+<ul>
+   <li>
+      <div class="flex">
+         <img 
+            src="https://i.pravatar.cc/40" 
+            alt=""
+         >
+
+         John Doe
+      </div>
+   </li>
+</ul>
