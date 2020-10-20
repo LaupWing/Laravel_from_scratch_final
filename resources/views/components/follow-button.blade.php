f8ce8737c19e5aaa7b5d010459fe2981dea256eb
LaupWing <loc_strict@hotmail.com>
2020-10-20 18:50:21 +0200

    isnot

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,9 +1,11 @@
-<form method="POST" action="/profiles/{{$user->name}}/follow">
-   @csrf
-   <button
-      type="submit"
-      class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
-   >
-      {{auth()->user()->following($user) ? 'Unfollow me' : 'Follow Me'}}
-   </button>
-</form>
+@if (auth()->user()->isNot($user))
+   <form method="POST" action="/profiles/{{$user->name}}/follow">
+      @csrf
+      <button
+         type="submit"
+         class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
+      >
+         {{auth()->user()->following($user) ? 'Unfollow me' : 'Follow Me'}}
+      </button>
+   </form>
+@endif
