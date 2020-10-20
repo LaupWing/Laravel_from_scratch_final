8777f83f3586e579188e98ad739f5d773b0af571
LaupWing <loc_strict@hotmail.com>
2020-10-20 14:15:28 +0200

    a62

--- app/Followable.php
+++ app/Followable.php
@@ -11,9 +11,22 @@ trait Followable
       return $this->follows()->save($user);
    }
 
+   public function unfollow(User $user)
+   {
+      return $this->follows()->detach($user);
+   }
+
    public function follows()
    {
       return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');
+   }
+
+   public function toggleFollower(User $user)
+   {
+      if ($this->following($user)) {
+         return $this->unfollow($user);
+      }
+      return $this->follow($user);
    }
 
    public function following(User $user)
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,35 +1,30 @@
 <x-app>
    <header class="mb-6 relative">
-      <img 
-         src="/images/default-profile-banner.jpg" 
-         class="mb-2"
-         alt=""
-      >
-      <div class="flex justify-between items-center mb-4">
+      <div class="relative">
+         <img 
+            src="/images/default-profile-banner.jpg" 
+            class="mb-2"
+            alt=""
+         >
+         <img 
+            src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" 
+            alt="avatar"
+            class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
+            style="width: 150px; left: 50%"
+         >
+      </div>
+      <div class="flex justify-between items-center mb-6">
          <div>
             <h2 class="font-bold text-2xl mb-2">{{$user->name}}</h2>
             <p class="text-sm mb-0">Joined {{$user->created_at->diffForHumans()}}</p>
          </div>
          <div class="flex">
             <a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">Edit Profile</a>
-            <form method="POST" action="/profiles/{{$user->name}}/follow">
-               @csrf
-               <button
-                  type="submit"
-                  class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
-               >
-                  {{auth()->user()->following($user) ? 'Unfollow me' : 'Follow Me'}}
-               </button>
-            </form>
+            <x-follow-button :user="$user"></x-follow-button>
          </div>
       </div>
       <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab voluptatum, facere numquam, ad veniam quibusdam minus similique aut eum labore quisquam nesciunt a eveniet consectetur mollitia, tempore vel tenetur aliquam.</p>
-      <img 
-         src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" 
-         alt="avatar"
-         class="rounded-full mr-2 absolute"
-         style="width: 150px; left: calc(50% - 75px); top: 35%;"
-      >
+      
    </header>
    @include('_timeline',[
       'tweets' => $user->tweets
