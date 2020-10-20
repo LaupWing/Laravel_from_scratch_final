836d61931fab4eac038ad2cd5a50d05741009544
LaupWing <loc_strict@hotmail.com>
2020-10-20 09:46:21 +0200

    added bannner

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -2,10 +2,10 @@
    <div class="mr-2 flex-shrink-0">
       <a href="{{ route('profile', $tweet->user->name)}}">
          <img 
-         width="50px"
-         src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" 
-         alt="avatar"
-         class="rounded-full mr-2"
+            width="50px"
+            src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" 
+            alt="avatar"
+            class="rounded-full mr-2"
          >
       </a>
    </div>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,8 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-   <header>
-      
+   <header class="mb-6">
+      <img 
+         src="/images/default-profile-banner.jpg" 
+         class="mb-2"
+         alt=""
+      >
+      <div class="flex justify-between items-center">
+         <div>
+            <h2 class="font-bold text-2xl mb-2">{{$user->name}}</h2>
+            <p class="text-sm mb-0">Joined {{$user->created_at->diffForHumans()}}</p>
+         </div>
+         <div>
+            <a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">Edit Profile</a>
+            <a href="" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">Follow Me</a>
+         </div>
+      </div>
+      <img 
+         width="50px"
+         src="https://breakthrough.org/wp-content/uploads/2018/10/default-placeholder-image.png" 
+         alt="avatar"
+         class="rounded-full mr-2"
+         >
    </header>
    @include('_timeline',[
       'tweets' => $user->tweets
